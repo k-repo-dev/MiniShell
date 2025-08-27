a28c866a108e0b04d5586d8ce8a2ae934bfb5dde
krepo <krepo@student.hive.fi>
2025-08-27 15:26:41 +0300

    started the input and history handling

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,22 @@
-#include "minishell.h"
+#include "../incls/minishell.h"
 
 int	main(int ac, char *av[])
 {
+	char *line;
+	
+	(void)ac;
+	(void)av;
+	while (1)
+	{
+		line = readline("my_prompt> ");
+		if (line == NULL)
+		{
+			printf("exit\n");
+			break ;
+		}
+		if (*line)
+			add_history(line);
+		free(line);
+	}
 	return (0);
 }
