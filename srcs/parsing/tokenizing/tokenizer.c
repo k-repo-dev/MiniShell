c16a42c36e37f5a4c5ba81679fc7ccdfbfd808a9
krepo <krepo@student.hive.fi>
2025-09-08 16:20:11 +0300

    built first version of tokenizer function

--- srcs/parsing/tokenizing/tokenizer.c
+++ srcs/parsing/tokenizing/tokenizer.c
@@ -3,16 +3,60 @@
 t_token	*tokenizer(const char *line, t_arena *arena)
 {
 	t_token		*head;
-	t_token		*current;
+	t_token		*last;
+	t_token		*new;
 	const char	*cursor;
+	const char	*token_end;
 
 	head = NULL;
-	current = NULL;
+	last = NULL;
 	cursor = line;
-	while (*cursor != '\0')
+	while (*cursor)
 	{
-		while (ft_iswhitespace(*cursor) == 1)
+		while (*cursor == ' ' || *cursor == '\t')
 			cursor++;
-		identify_token_type(*)
+		if (!*cursor)
+			break;
+		token_end = find_token_end(cursor);
+		if (!token_end)
+			return (NULL);
+		new = (t_token *)alloc_arena(arena, sizeof(t_token));
+		if (!new)
+			return (NULL);
+		new->value = arena_strdup(arena, cursor);
+		new->type = ARG_TOKEN;
+		new->next = NULL;
+		if (!head)
+			head = new;
+		else
+			last->next = new;
+		last = new;
+		cursor = token_end;
 	}
+	return (head);
+}
+
+const char *find_token_end(const char *start)
+{
+	const char	*end;
+
+	if (*start == '|' || *start == '<' || *start == '>')
+		return (start + 1);
+	if (*start == '\'' || *start == '"')
+	{
+		end = start + 1;
+		while (*end && *end != *start)
+			end++;
+		if (!*end)
+		{
+			ft_putstr_fd("Error: unclosed quote\n", 2);
+			return (NULL);
+		}
+		return (end + 1);
+	}
+	end = start;
+	while (*end && *end != ' ' && *end != '\t' && *end != '|'
+			&& *end != '<' && *end != '>')
+		end++;
+	return (end);
 }
