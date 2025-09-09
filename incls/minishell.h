9dcc884f3b2910b6a4e616a79fb65f29b747d4de
pnurmi <pnurmi.zt@gmail.com>
2025-09-09 09:02:23 +0300

    added execution directories

--- incls/minishell.h
+++ incls/minishell.h
@@ -1,8 +1,14 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
+# include <sys/wait.h>
 # include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
