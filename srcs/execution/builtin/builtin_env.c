d79771f3f8e1b8e3d626b3849a0a467207aeb2ef
slowTake <pnurmi.zt@gmail.com>
2025-09-27 09:04:08 +0300

    cleaned up unused functions

--- srcs/execution/builtin/builtin_env.c
+++ srcs/execution/builtin/builtin_env.c
@@ -25,26 +25,3 @@ int	builtin_cd(t_command *cmd)
 	}
 	return (0);
 }
-
-// sort by ascii
-// int	builtin_export(t_command *cmd, t_env **envp)
-// {
-// 	int	i;
-
-// 	i = 1;
-// 	if (!cmd->args[1])
-// 		add_env_node(env);
-// 	return (0);
-// }
-
-// // int	builtin_unset(t_command *cmd, char **envp);
-
-// void	print_export(t_env *env)
-// {
-// 	while (env)
-// 	{
-// 		// ordered by ascii
-// 		printf("%s", env->value);
-// 		env = env->next;
-// 	}
-// }
