9c463802fc3710ee021d7dd592184bc74b8ba5ec
pnurmi <pnurmi.zt@gmail.com>
2025-10-01 11:48:42 +0300

    pipes working with new main and cmd parsing

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,10 @@
 #include "../incls/prototypes.h"
 
 static int	run_minishell_loop(t_env **env_list);
+static int	process_input(t_env **env_list, int *exit_status);
+static int	execute_line_logic(t_env **env_list, char **line, int *exit_status);
+static int	handle_exit_command(t_command *cmd_list, int exit_status,
+				int *final_exit_code);
 
 int	main(int ac, char *av[], char **envp)
 {
@@ -12,93 +16,113 @@ int	main(int ac, char *av[], char **envp)
 	if (init_sigaction(handle_sigint) == -1)
 		return (1);
 	env_list = init_env_list(envp);
+	printf("init_env_list done\n");
 	exit_code = run_minishell_loop(&env_list);
+	printf("run_minishell_loop done, now freeing the env_list\n");
 	if (env_list)
 		free_env_list(env_list);
+	printf("bye-bye\n");
 	return (exit_code);
 }
 
 static int	run_minishell_loop(t_env **env_list)
 {
-	char		*line;
-	t_token		*token_list;
-	t_command	*command_list;
-	t_arena		arena;
-	int			exit_status;
-	int			final_exit_code;
+	int	exit_status;
+	int	final_exit_code;
 
 	exit_status = 0;
 	while (1)
 	{
-		ft_memset(&arena, 0, sizeof(t_arena));
-		command_list = NULL;
-		// if (isatty(fileno(stdin))) //<<<<<<<<<< minishell tester
-		line = readline("my_prompt> ");
-		// else
-		// {
-		// 	line = get_next_line(fileno(stdin));
-		// 	line = ft_strtrim(line, "\n");
-		// 	free(line);
-		// }
-		if (line == NULL)
-		{
-			// printf("exit\n"); // remove after
-			rl_clear_history();
-			if (command_list)
-				cleanup_redirs(command_list);
-			free_arena(&arena);
-			free_env_list(*env_list);
-			*env_list = NULL;
-			return (exit_status);
-		}
-		if (*line == '\0')
-		{
-			free(line);
-			continue ;
-		}
-		if (*line)
-			add_history(line);
-		init_arena(&arena, (ft_strlen(line) * 2) + 4096);
-		token_list = tokenizer(line, &arena);
-		if (token_list == NULL)
-		{
-			printf("here1\n");
-			exit_status = handle_error(E_SYNTAX_ERROR, NULL);
-			free(line);
-			continue ;
-		}
-		command_list = parse_commands(token_list, &arena);
-		if (command_list == NULL)
-		{
-			printf("exiting here2\n");
-			exit_status = handle_error(E_SYNTAX_ERROR, NULL);
-			free(line);
-			continue ;
-		}
-		expand_commands(command_list, &arena, exit_status, *env_list);
-		exit_status = parent_loop(command_list, env_list, exit_status);
-		if (exit_status == -1 || exit_status == -2)
-		{
-			if (exit_status == -2)
-				final_exit_code = 2;
-			else
-			{
-				if (command_list && command_list->args && command_list->args[1]
-					&& is_numeric(command_list->args[1]))
-					final_exit_code = (int)ft_atoi(command_list->args[1]);
-				else
-					final_exit_code = exit_status;
-			}
-			free(line);
-			free_arena(&arena);
-			rl_clear_history();
-			free_env_list(*env_list);
-			*env_list = NULL;
+		final_exit_code = process_input(env_list, &exit_status);
+		if (final_exit_code != -1)
 			return (final_exit_code);
-		}
-		printf("here3\n");
-		free(line);
-		free_arena(&arena);
+		printf("process_input run correctly\n");
 	}
-	return (exit_status);
+}
+
+static int	process_input(t_env **env_list, int *exit_status)
+{
+	char	*line;
+	int		command_result;
+
+	line = readline("my_prompt> ");
+	if (line == NULL)
+	{
+		printf("exit\n");
+		rl_clear_history();
+		return (*exit_status);
+	}
+	if (*line == '\0')
+	{
+		free(line);
+		return (-1);
+	}
+	if (*line)
+		add_history(line);
+	command_result = execute_line_logic(env_list, &line, exit_status);
+	free(line);
+	if (command_result != -1)
+		return (command_result);
+	return (-1);
+}
+
+static int	execute_line_logic(t_env **env_list, char **line, int *exit_status)
+{
+	t_arena		arena;
+	t_token		*token_list;
+	t_command	*cmd_list;
+	int			command_status;
+	int			final_exit_code_signal;
+
+	ft_memset(&arena, 0, sizeof(t_arena));
+	init_arena(&arena, (ft_strlen(*line) * 2) + 4096);
+	token_list = tokenizer(*line, &arena);
+	if (token_list == NULL)
+	{
+		free_arena(&arena);
+		return (handle_error(E_SYNTAX_ERROR, NULL));
+	}
+	cmd_list = parse_commands(token_list, &arena);
+	if (cmd_list == NULL)
+	{
+		free_arena(&arena);
+		return (handle_error(E_SYNTAX_ERROR, NULL));
+	}
+	expand_commands(cmd_list, &arena, *exit_status, *env_list);
+	command_status = parent_loop(cmd_list, env_list, *exit_status);
+	final_exit_code_signal = -1;
+	handle_exit_command(cmd_list, command_status, &final_exit_code_signal);
+	free_arena(&arena);
+	if (final_exit_code_signal != -1)
+		return (final_exit_code_signal);
+	*exit_status = command_status;
+	return (-1);
+}
+
+static int	handle_exit_command(t_command *cmd_list, int exit_status,
+		int *final_exit_code)
+{
+	t_command	*cmd;
+
+	*final_exit_code = -1;
+	cmd = cmd_list;
+	if (cmd && cmd->args && cmd->args[0] && ft_strcmp(cmd->args[0],
+			"exit") == 0)
+	{
+		if (exit_status == -1)
+		{
+			if (cmd->args[1] && is_numeric(cmd->args[1]))
+				*final_exit_code = (int)ft_atoi(cmd->args[1]) & 0xFF;
+			else
+				*final_exit_code = 0;
+			rl_clear_history();
+			return (1);
+		}
+	}
+	else if (exit_status == 2)
+	{
+		*final_exit_code = 2;
+		return (1);
+	}
+	return (0);
 }
--- srcs/parsing/commands/commands.c
+++ srcs/parsing/commands/commands.c
@@ -1,6 +1,12 @@
 #include "../../../incls/prototypes.h"
 
+static int			process_token(t_command **current_cmd,
+						t_token **current_token, t_command **cmd_head,
+						t_arena *arena);
+static int			redir_handler(t_command *cmd, t_token **token,
+						t_arena *arena);
 static t_command	*create_and_link_cmd(t_command **cmd_head, t_arena *arena);
+static t_command	*get_last_cmd(t_command *cmd_head);
 
 t_command	*parse_commands(t_token *token_head, t_arena *arena)
 {
@@ -9,45 +15,55 @@ t_command	*parse_commands(t_token *token_head, t_arena *arena)
 	t_token		*current_token;
 	int			error_flag;
 
-	error_flag = 0;
 	cmd_head = NULL;
 	current_cmd = NULL;
 	current_token = token_head;
+	error_flag = 0;
 	while (current_token && !error_flag)
 	{
-		if (!current_cmd)
-		{
-			current_cmd = create_and_link_cmd(&cmd_head, arena);
-			if (!current_cmd)
-			{
-				printf("we here? \n");
-				return (NULL);
-			}
-		}
-		if (current_token->type == PIPE_TOKEN)
-			current_cmd = NULL;
-		else if (current_token->type == LESS_TOKEN
-			|| current_token->type == GREAT_TOKEN
-			|| current_token->type == HEREDOC_TOKEN
-			|| current_token->type == APPEND_TOKEN)
-		{
-			if (add_redir_to_cmd(current_cmd, &current_token, arena) != 0)
-			{
-				error_flag = 1;
-				continue ;
-			}
-		}
-		else
-		{
-			add_arg_to_cmd(current_cmd, current_token, arena);
+		error_flag = process_token(&current_cmd, &current_token, &cmd_head,
+				arena);
+		if (error_flag == 0 && current_token)
 			current_token = current_token->next;
-		}
 	}
 	if (error_flag)
-	{
 		return (NULL);
-	}
 	return (cmd_head);
+}
+
+static int	process_token(t_command **current_cmd, t_token **current_token,
+		t_command **cmd_head, t_arena *arena)
+{
+	t_token	*token;
+
+	token = *current_token;
+	if (!(*current_cmd))
+	{
+		*current_cmd = create_and_link_cmd(cmd_head, arena);
+		if (!(*current_cmd))
+			return (1);
+	}
+	if (token->type == PIPE_TOKEN)
+		*current_cmd = NULL;
+	else if (token->type == LESS_TOKEN || token->type == GREAT_TOKEN
+		|| token->type == HEREDOC_TOKEN || token->type == APPEND_TOKEN)
+	{
+		if (redir_handler(*current_cmd, current_token, arena) != 0)
+			return (1);
+	}
+	else
+		add_arg_to_cmd(*current_cmd, token, arena);
+	return (0);
+}
+
+static int	redir_handler(t_command *cmd, t_token **token, t_arena *arena)
+{
+	if (!(*token)->next)
+		return (handle_error(E_SYNTAX_ERROR, "missing filename"));
+	*token = (*token)->next;
+	if (add_redir_to_cmd(cmd, token, arena) != 0)
+		return (1);
+	return (0);
 }
 
 static t_command	*create_and_link_cmd(t_command **cmd_head, t_arena *arena)
@@ -55,24 +71,26 @@ static t_command	*create_and_link_cmd(t_command **cmd_head, t_arena *arena)
 	t_command	*new_cmd;
 	t_command	*last_cmd;
 
-	printf("we here too? \n");
 	new_cmd = alloc_arena(arena, sizeof(t_command));
 	if (!new_cmd)
 		return (NULL);
 	ft_memset(new_cmd, 0, sizeof(t_command));
-	new_cmd->next = NULL;
-	new_cmd->args = NULL;
-	new_cmd->redirs = NULL;
 	if (!*cmd_head)
 		*cmd_head = new_cmd;
 	else
 	{
-		printf("what about here? \n");
-		last_cmd = *cmd_head;
-		while (last_cmd->next)
-			last_cmd = last_cmd->next;
+		last_cmd = get_last_cmd(*cmd_head);
 		last_cmd->next = new_cmd;
 	}
-	printf("I guess here too? \n");
 	return (new_cmd);
+}
+
+static t_command	*get_last_cmd(t_command *cmd_head)
+{
+	t_command	*last_cmd;
+
+	last_cmd = cmd_head;
+	while (last_cmd->next)
+		last_cmd = last_cmd->next;
+	return (last_cmd);
 }
