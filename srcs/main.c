edbc4118cc905f4a1a923b88b7e0add253d5869a
pnurmi <pnurmi.zt@gmail.com>
2025-10-01 09:18:18 +0300

    commented out tester lines

--- srcs/main.c
+++ srcs/main.c
@@ -32,70 +32,70 @@ static int	run_minishell_loop(t_env **env_list)
 	{
 		ft_memset(&arena, 0, sizeof(t_arena));
 		command_list = NULL;
-		if (isatty(fileno(stdin))) //<<<<<<<<<< minishell tester // Remove after
-			line = readline("my_prompt> ");
+		// if (isatty(fileno(stdin))) //<<<<<<<<<< minishell tester
+		line = readline("my_prompt> ");
+		// else
+		// {
+		// 	line = get_next_line(fileno(stdin));
+		// 	line = ft_strtrim(line, "\n");
+		// 	free(line);
+	}
+	if (line == NULL)
+	{
+		// printf("exit\n"); // remove after
+		rl_clear_history();
+		if (command_list)
+			cleanup_redirs(command_list);
+		free_arena(&arena);
+		free_env_list(*env_list);
+		*env_list = NULL;
+		return (exit_status);
+	}
+	if (*line == '\0')
+	{
+		free(line);
+		continue ;
+	}
+	if (*line)
+		add_history(line);
+	init_arena(&arena, (ft_strlen(line) * 2) + 4096);
+	token_list = tokenizer(line, &arena);
+	if (token_list == NULL)
+	{
+		exit_status = handle_error(E_SYNTAX_ERROR, NULL);
+		free(line);
+		continue ;
+	}
+	command_list = parse_commands(token_list, &arena);
+	if (command_list == NULL)
+	{
+		exit_status = handle_error(E_SYNTAX_ERROR, NULL);
+		free(line);
+		continue ;
+	}
+	expand_commands(command_list, &arena, exit_status, *env_list);
+	exit_status = parent_loop(command_list, env_list, exit_status);
+	if (exit_status == -1 || exit_status == -2)
+	{
+		if (exit_status == -2)
+			final_exit_code = 2;
 		else
 		{
-			line = get_next_line(fileno(stdin));
-			line = ft_strtrim(line, "\n");
-			free(line);
-		}
-		if (line == NULL)
-		{
-			// printf("exit\n"); // remove after
-			rl_clear_history();
-			if (command_list)
-				cleanup_redirs(command_list);
-			free_arena(&arena);
-			free_env_list(*env_list);
-			*env_list = NULL;
-			return (exit_status);
-		}
-		if (*line == '\0')
-		{
-			free(line);
-			continue ;
-		}
-		if (*line)
-			add_history(line);
-		init_arena(&arena, (ft_strlen(line) * 2) + 4096);
-		token_list = tokenizer(line, &arena);
-		if (token_list == NULL)
-		{
-			exit_status = handle_error(E_SYNTAX_ERROR, NULL);
-			free(line);
-			continue ;
-		}
-		command_list = parse_commands(token_list, &arena);
-		if (command_list == NULL)
-		{
-			exit_status = handle_error(E_SYNTAX_ERROR, NULL);
-			free(line);
-			continue ;
-		}
-		expand_commands(command_list, &arena, exit_status, *env_list);
-		exit_status = parent_loop(command_list, env_list, exit_status);
-		if (exit_status == -1 || exit_status == -2)
-		{
-			if (exit_status == -2)
-				final_exit_code = 2;
+			if (command_list && command_list->args && command_list->args[1]
+				&& is_numeric(command_list->args[1]))
+				final_exit_code = (int)ft_atoi(command_list->args[1]);
 			else
-			{
-				if (command_list && command_list->args && command_list->args[1]
-					&& is_numeric(command_list->args[1]))
-					final_exit_code = (int)ft_atoi(command_list->args[1]);
-				else
-					final_exit_code = exit_status;
-			}
-			free(line);
-			free_arena(&arena);
-			rl_clear_history();
-			free_env_list(*env_list);
-			*env_list = NULL;
-			return (final_exit_code);
+				final_exit_code = exit_status;
 		}
 		free(line);
 		free_arena(&arena);
+		rl_clear_history();
+		free_env_list(*env_list);
+		*env_list = NULL;
+		return (final_exit_code);
 	}
-	return (exit_status);
+	free(line);
+	free_arena(&arena);
+}
+return (exit_status);
 }
