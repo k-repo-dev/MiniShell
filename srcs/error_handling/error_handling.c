588046c434b717ade604cce464529a87f4a2c9f4
krepo <krepo@student.hive.fi>
2025-10-03 16:13:19 +0300

    made error message resamble what bash is giving

--- srcs/error_handling/error_handling.c
+++ srcs/error_handling/error_handling.c
@@ -7,6 +7,13 @@ static int	handle_unhandled_error(void);
 
 int	handle_error(t_error_type type, const char *arg)
 {
+	if (type == E_SYNTAX_ERROR)
+	{
+		ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+		ft_putstr_fd((char *)arg, 2);
+		ft_putstr_fd("'\n", 2);
+		return (258);
+	}
 	ft_putstr_fd("minishell: ", 2);
 	if (arg && *arg && type != E_CMD_NOT_FOUND && type != E_EMPTY_CMD
 		&& type != E_FILE_NOT_FOUND && type != E_FILE_PERMISSION)
@@ -32,7 +39,7 @@ int	handle_error(t_error_type type, const char *arg)
 		else
 			return (handle_file_error(arg, "Permission denied"));
 	}
-	if (type == E_PERMISSION_DENIED || type == E_SYNTAX_ERROR)
+	if (type == E_PERMISSION_DENIED)
 		return (handle_system_errors(type));
 	else if (type == E_CD_OLDPWD_UNSET || type == E_CD_TOO_MANY_ARGS)
 		return (handle_cd_errors(type));
@@ -48,11 +55,6 @@ static int	handle_system_errors(t_error_type type)
 	{
 		ft_putstr_fd("Permission denied\n", 2);
 		return (126);
-	}
-	else if (type == E_SYNTAX_ERROR)
-	{
-		ft_putstr_fd("syntax error\n", 2);
-		return (258);
 	}
 	return (handle_unhandled_error());
 }
@@ -74,6 +76,7 @@ static int	handle_cd_errors(t_error_type type)
 
 static int	handle_exit_errors(t_error_type type, const char *arg)
 {
+	(void)arg;
 	if (type == E_EXIT_TOO_MANY_ARGS)
 	{
 		ft_putstr_fd("too many arguments\n", 2);
@@ -81,7 +84,7 @@ static int	handle_exit_errors(t_error_type type, const char *arg)
 	}
 	else if (type == E_EXIT_NUMERIC_REQUIRED)
 	{
-		ft_putstr_fd((char *)arg, 2);
+		ft_putstr_fd("exit: ", 2);
 		ft_putstr_fd(": numeric argument required\n", 2);
 		return (2);
 	}
